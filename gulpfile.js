fda55e260793bb5bd15d1e5f003fd15e364e398f
azhar0315 <azharrasheed0315@gmail.com>
2017-03-10 17:29:23 +0500

    Revert "gulp file change"
    This reverts commit 6a8c49192ed6730467405f85b60545b7ee876e7a.

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,59 @@
 /* File: gulpfile.js */
 
 // grab our gulp packages
-var gulp = require('gulp'),
+var gulp  = require('gulp'),
     gutil = require('gulp-util'),
     runSequence = require('run-sequence'),
-    del = require('del'),
+        del = require('del'),   
     browserSync = require('browser-sync').create();
 
 
 //browserSync 
 gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: 'dist'
-        },
-    })
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    },
+  })
 });
 
 
 
 //copying scripts
 gulp.task('copying-scripts', function() {
-    return gulp.src('client/static/**/*')
-        .pipe(gulp.dest('dist/'))
+  return gulp.src('client/static/**/*')
+  .pipe(gulp.dest('dist/'))
 });
 
 //copying index
 gulp.task('index', function() {
-    return gulp.src('client/index.html')
-        .pipe(gulp.dest('dist/'))
+  return gulp.src('client/index.html')
+  .pipe(gulp.dest('dist/'))
 });
 //del the dist first
 gulp.task('clean:dist', function() {
-    return del.sync('dist');
+  return del.sync('dist');
 });
 
 //watch task
-gulp.task('watch', ['browserSync'], function() {
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('dist/**/*', browserSync.reload);
+gulp.task('watch', ['browserSync'], function (){
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('dist/**/*', browserSync.reload); 
 });
 
 // gulp sequence
-gulp.task('build', function(callback) {
-    runSequence('clean:dist', [
-            'copying-scripts',
-            'browserSync',
-            'watch'
-        ],
+gulp.task('build', function (callback) {
+  runSequence(
+    [ 
+    
+    'browserSync',
+    'watch'
+    ],
 
-        callback
-    )
+    callback
+  )
 });
 
 
 // create a default task and just log a message
-gulp.task('default', ['build']);
+gulp.task('default',['build']);
